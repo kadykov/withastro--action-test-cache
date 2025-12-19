f3c9b077c7ff353c2836156acbfca23eb0a203d0
Aleksandr Kadykov <git@kadykov.com>
2025-12-19 17:23:51 +0000

    build(config): add site URL to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://github.kadykov.com/withastro--action-test-cache/",
   image: {
     domains: ["picsum.photos"],
-  }
+  },
 });
