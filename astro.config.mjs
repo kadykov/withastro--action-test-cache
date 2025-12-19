db38be3cdde4cc12f87ee1189aae6a1bec095a2a
Aleksandr Kadykov <git@kadykov.com>
2025-12-19 17:47:21 +0000

    ci(workflows): add github-pages environment to deploy job and update site config
    Update Astro config to set base path for GitHub Pages deployment and re-add environment configuration to the deploy workflow for proper page URL handling.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://github.kadykov.com/withastro--action-test-cache/",
+  site: "https://github.kadykov.com",
+  base: "/withastro--action-test-cache",
   image: {
     domains: ["picsum.photos"],
   },
